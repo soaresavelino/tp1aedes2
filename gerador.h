a09f96243b3804073246f7339dca9fd2b682cb50
Gabriel Soares <gabriel.20036111@gmail.com>
2025-11-15 10:10:24 -0300

    Removendo arquivos antigos

--- gerador.h
+++
@@ -1,9 +0,0 @@
-#ifndef GERADOR_H
-#define GERADOR_H
-
-#include "aluno.h"
-
-Aluno *gerarListaAlunos(int n);
-void salvarOriginal(Aluno *alunos, int n);
-
-#endif
--- salvar_original.h
+++
@@ -1,8 +0,0 @@
-#ifndef SALVAR_ORIGINAL_H
-#define SALVAR_ORIGINAL_H
-
-#include "aluno.h"
-
-void salvarOriginal(Aluno *alunos, int n);
-
-#endif
